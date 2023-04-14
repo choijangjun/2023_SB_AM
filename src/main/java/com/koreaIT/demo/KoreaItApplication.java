1a29d7f4859be00e703029991858baf3af0c2e05
choijangjun <jangjun9644@gmail.com>
2023-04-14 13:56:53 +0900

    ArticleController doAdd, getArticles

---
+++ src/main/java/com/koreaIT/demo/KoreaItApplication.java
@@ -0,0 +1,13 @@
+package com.koreaIT.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoreaItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoreaItApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/koreaIT/demo/KoreaItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koreaIT.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoreaItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
